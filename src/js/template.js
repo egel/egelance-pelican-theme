9bcb1950f43cd4c860c1d5de3b87783b6fb8ded0
Maciej Sypień <maciejsypien@gmail.com>
2015-12-16 17:39:07 +0100

    update after merge

--- src/js/template.js
+++ src/js/template.js
@@ -4,9 +4,9 @@
 //'h1, h2, h3, h4, h5, h6' adding anchors to all headings.
 anchors.options = {
   placement:  'left',
-  icon: '¶'
+  icon:       '¶'
 };
 
-anchors.add('#content > h2, h3, h4, h5, h6');
+anchors.add('.single-article > h2, h3, h4, h5, h6');
 anchors.remove('.no-anchor');
 anchors.remove('#sidebar > h1, h2, h3, h4, h5, h6');
